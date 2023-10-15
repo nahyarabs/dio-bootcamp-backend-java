c79dde41e042a78945d086f50c4bccb5e1febfe2
nahyarabs <nahyarabs@gmail.com>
2023-10-15 20:13:49 -0300

    criando repositorio remoto

---
+++ src/main/java/dio/santander/bootcampbackendjava/BootcampBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package dio.santander.bootcampbackendjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampBackendJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/santander/bootcampbackendjava/BootcampBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.santander.bootcampbackendjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
